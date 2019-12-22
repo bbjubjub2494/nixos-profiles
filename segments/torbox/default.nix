695de66543472878c3cac987116ed42b70994c53
Louis Bettens <louis@bettens.info>
2019-12-22 18:21:12 +0100

    improve torbox

--- segments/torbox/default.nix
+++ segments/torbox/default.nix
@@ -11,14 +11,30 @@ in
       users.users.user = {
         isNormalUser = true;
       };
+
+      services.mingetty.autologinUser = "user";
+
       networking.proxy.default = "socks5h://${hostAddress}:9063";
+
+      services.tor.torsocks = {
+        enable = true;
+        server = "${hostAddress}:9050";
+        fasterServer = "${hostAddress}:9063";
+      };
     };
 
     # need to xhost and stuff
-    bindMounts.x11_sockets = rec {
-      hostPath = "/tmp/.X11-unix";
-      mountPoint = hostPath;
-      isReadOnly = true;
+    bindMounts = {
+      x11_sockets = rec {
+        hostPath = "/tmp/.X11-unix";
+        mountPoint = hostPath;
+        isReadOnly = true;
+      };
+      home = {
+        hostPath = "/home/louis/torbox";
+        mountPoint = "/home/user";
+        isReadOnly = false;
+      };
     };
   };
 
