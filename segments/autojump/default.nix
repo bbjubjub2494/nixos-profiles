0b143faca5d4ed8e8b8b30bef36c42cd6f118676
Louis Bettens <louis@bettens.info>
2020-02-19 23:18:17 +0100

    Add CDPATH

--- segments/autojump/default.nix
+++ segments/autojump/default.nix
@@ -1,5 +1,9 @@
 # Enable and configure autojump (shortcut for changing current directory)
+#
+# Also an additional keystroke saver: the home directory is in the CDPATH
 
 {
   programs.autojump.enable = true;
+
+  environment.variables.CDPATH = ["." "~"];
 }
