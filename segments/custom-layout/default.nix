eafb6e519281af8574541cc537d4be832e1a623d
Louis Bettens <louis@bettens.info>
2020-09-12 21:26:17 +0200

    custom-layout: add X-Bows layout

--- segments/custom-layout/default.nix
+++ segments/custom-layout/default.nix
@@ -1,12 +1,19 @@
 # Enable and configure my keyboard layout. (also on the tty!)
 
 {
-  services.xserver.extraLayouts.custom = {
-    description = "my own custom layout";
-    symbolsFile = symbols/custom;
-    languages = [ "en" "fr" "de" ];
+  services.xserver.extraLayouts = {
+    custom_tek = {
+      description = "my own custom layout (Teck)";
+      symbolsFile = symbols/custom_tek;
+      languages = [ "en" "fr" "de" ];
+    };
+
+    custom_xbows = {
+      description = "my own custom layout (X-Bows)";
+      symbolsFile = symbols/custom_xbows;
+      languages = [ "en" "fr" "de" ];
+    };
   };
 
-  services.xserver.layout = "custom";
-  services.xserver.xkbModel = "teck227";
+  services.xserver.layout = "custom_xbows";
 }
